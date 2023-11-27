2339e70bc734c9085c1aff5b07ff421293f1abfb
Berenshteyn Arkadiy <92229986+akamaaru@users.noreply.github.com>
2023-11-27 13:17:02 +0300

    ClassTasks done.kt

--- Seminar3-kotlin-basics/src/main/kotlin/ClassTasks.kt
+++ Seminar3-kotlin-basics/src/main/kotlin/ClassTasks.kt
@@ -1,3 +1,5 @@
+import kotlin.math.PI
+
 /**
  * Задание 1: Объявите классы Circle и Square – наследников базового (Point).
  * Реализуйте readonly свойство, вычисляющее площадь каждой фигуры, а также функцию display()
@@ -6,22 +8,37 @@
  * (3 балла)
  */
 
-class Point(val X: Double, val Y: Double) {
+open class Point(val X: Double, val Y: Double) {
+    open val area: Double = 0.0
 
-    val area: Double = 0.0
+    open fun display() {
+        println("S = $area, X = $X, Y = $Y")
+    }
+}
 
-    fun display() {
-        // Формат вывода – S = ... , X = ... , Y = ...
+class Circle(X: Double, Y: Double, val R: Double) : Point(X, Y) {
+    override val area: Double = PI * R * R
+
+    override fun display() {
+        println("S = $area, X = $X, Y = $Y, R = $R")
+    }
+}
+
+class Square(X: Double, Y: Double, val A: Double) : Point(X, Y) {
+    override val area: Double = A * A
+
+    override fun display() {
+        println("S = $area, X = $X, Y = $Y, A = $A")
     }
 }
 
 // Используйте эту функцию для запуска кода
 // Раскомментируйте нужные участки в процессе реализации
 fun main() {
-//    val p = Point(34, 56)
-//    val c = Circle(46, 34, 7)
-//    val s = Square(46, 34, 8)
-//    p.display()
-//    c.display()
-//    s.display()
+    val p = Point(34.0, 56.0)
+    val c = Circle(46.0, 34.0, 7.0)
+    val s = Square(46.0, 34.0, 8.0)
+    p.display()
+    c.display()
+    s.display()
 }
